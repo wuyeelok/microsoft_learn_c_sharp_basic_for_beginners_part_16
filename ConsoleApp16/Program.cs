9020f3bd73acd6d16907cb113715aec44ad5faee
wuyeelok <wuyeelok@gmail.com>
2025-02-21 23:23:34 +0800

    orderby in query

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -3,13 +3,14 @@ var p = 16;
 Console.WriteLine($"Hello, World! Part {p}");
 
 // Specify the data source.
-List<int> scores = new List<int> { 97, 92, 81, 60 };
+List<int> scores = new List<int> { 180, 2, 97, 92, 81, 60, 300 };
 
 
 // Define the query expression.
 IEnumerable<int> scoreQuery =
     from score in scores
     where score > 80 && score % 2 == 0
+    orderby score descending
     select score;
 
 // Execute the query.
@@ -18,4 +19,4 @@ foreach (var i in scoreQuery)
     Console.Write(i + " ");
 }
 
-// Output: 92
+// Output: 300, 180, 92
