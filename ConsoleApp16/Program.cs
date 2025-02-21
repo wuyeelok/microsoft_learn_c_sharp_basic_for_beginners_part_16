50605112fb607a871bbeb91e94c60355834d65a8
wuyeelok <wuyeelok@gmail.com>
2025-02-21 23:29:35 +0800

    Learn count

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -7,16 +7,18 @@ List<int> scores = new List<int> { 180, 2, 97, 92, 81, 60, 300 };
 
 
 // Define the query expression.
-IEnumerable<int> scoreQuery =
+IEnumerable<string> scoreQuery =
     from score in scores
     where score > 80 && score % 2 == 0
     orderby score descending
-    select score;
+    select $"{score} ";
 
 // Execute the query.
-foreach (var i in scoreQuery)
+foreach (var s in scoreQuery)
 {
-    Console.Write(i + " ");
+    Console.Write(s);
 }
-
 // Output: 300, 180, 92
+Console.WriteLine();
+Console.WriteLine($"Number of scores: {scoreQuery.Count()}");
+// Output: 3
